03c42310752d10be004825755a113eb98d91f239
Himanshu Paliwal <hpaliwal364@gmail.com>
2025-07-27 16:25:57 +0530

    increases token expiration time

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/serverConfig.js";
 
 export const generateJwtToken = (payload) => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "15d" });
 };
 
 export const verifyToken = (token) => {
